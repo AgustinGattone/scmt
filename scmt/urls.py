943050754b44b04578f8f124c08f293b5e3199cb
Agustin <AgustinGattone@gmail.com>
2017-09-22 02:36:25 -0300

    Singup

--- scmt/urls.py
+++ scmt/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from . import views
-
+from scmt import views as core_views
 
 urlpatterns = [
     url(r'^$', views.persona_list),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
