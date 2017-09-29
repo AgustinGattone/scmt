1a6fe02ab60ebe370bda49f12b691de0860e94f2
Francoavila16 <franco_avila16@outlook.com>
2017-09-29 09:23:12 -0300

    Eliminados archivos cache y agregado registro

--- scmt/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-from scmt import views as core_views
-
-urlpatterns = [
-    url(r'^$', views.persona_list),
-    url(r'^signup/', core_views.signup, name='views.signup'),
-]
