fda6ec2a48401a59ad4ad8a88db3f20ca51df257
Gonzalo Fernandez <maaaki.rodriguez@gmail.com>
2017-09-28 22:05:08 -0300

    Cambios en el html y en las url

--- scmt/views.py
+++ scmt/views.py
@@ -20,7 +20,9 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('/home')
     else:
         form = SignUpForm()
     return render(request, 'scmt/signup.html', {'form': form})
+
+
