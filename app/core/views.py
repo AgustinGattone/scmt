da557ea372b2ff7019bc7a1419b908eb7406314d
Gonzalo Fernandez <maaaki.rodriguez@gmail.com>
2017-10-22 21:44:07 -0300

    Comienzo de modificacion de usuarios

--- app/core/views.py
+++ app/core/views.py
@@ -66,6 +66,19 @@ def handle_registro_empresa_form(request):
     else:
         return render(request, 'signup.html', {'form': form})
 
+def user_edit(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = RegistroDesocupado(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('post_detail', pk=user.pk)
+        else:
+            form = RegistroDesocupado(instance=post)
+        return render(request, 'scmt/user_edit.html', {'form': form})
+
 def persona_list(request):
     persona = Desocupado.objects.all
     return render(request, 'scmt/persona_list.html', {'personas': persona})
